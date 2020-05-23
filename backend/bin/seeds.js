98003cba525a498f1bdf2bddd9598e2fe5571940
Chad Hinds <chadrickj8@gmail.com>
2020-05-23 13:56:03 -0400

    Add backend folder

---
+++ backend/bin/seeds.js
@@ -0,0 +1,29 @@
+const csvFilePath = './drugsComTest_raw.csv'
+const csv = require('csvtojson')
+const Medicine = require('../models/Medicine');
+const User = require('../models/User');
+const users = require('./users')
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ironplate'
+console.log('Connecting DB to ', MONGODB_URI)
+
+mongoose
+    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch((err) => console.error('Error connecting to mongo', err));
+//Convert csv drug data to JSON and insert it
+csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+        console.log(jsonObj);
+    Medicine.insertMany(jsonObj) 
+       
+        /**
+         * [
+         * 	{a:"1", b:"2", c:"3"},
+         * 	{a:"4", b:"5". c:"6"}
+         * ]
+         */
+    })
+User.insertMany(users)
---
+++ backend/bin/users.js
@@ -0,0 +1,54 @@
+const users = [
+    {
+       identity:{
+        email: 'gamer_girl69@gmail.com',
+        name: 'Samantha Jay',
+        primaryPharmacy: 'CVS'} ,
+        conditions: ['Weight Loss', 'Narcolepsy', 'Acne', 'Pain'],
+        order: []
+    },
+    {
+        identity:{email: 'giovanni@gmail.com',
+        name: 'Giovanni Totti',
+        primaryPharmacy: "Leeroy Pharmacy"},
+        conditions: ['Depression', 'Weight Loss', 'Migraine Prevention'],
+        order: []
+    },
+    {
+        identity:{email: 'bradhindsight.com',
+        name: 'Bradford Chang',
+        primaryPharmacy: "Duane Reade"},
+        conditions: ['Acne', 'Cough'],
+        order: []
+    },
+    {
+        identity:{'weeeeeeeeeed@hotmail.com',
+        name: 'Steven A Smith',
+        primaryPharmacy: "Walgreens"}email: ,
+        conditions: ['Renal Cell Carcinoma', 'Bipolar Disorde'],
+        order: []
+    },
+    {
+       identity:{email: 'DJceecee@hotmail.com',
+        name: 'Cirian Murphy',
+        primaryPharmacy: 'CVS'} ,
+        conditions: ['Acne', 'Cough'],
+        order: []
+    },
+    {
+        identity: {email: 'slayer500@gmail.com',
+        name: 'Carl Watson',
+        primaryPharmacy: "Duane Reade"},
+        conditions: ['Chronic Myelogenous Leukemia'],
+        order: []
+    },
+    {
+       identity: { email: 'myFave@gmail.com',
+        name: 'Nicolle',
+        primaryPharmacy: "Walgreens"
+    },
+        conditions: ['Urinary Tract Infection', 'Insomnia'],
+        order: []
+    }
+]
+module.exports = users
