9d663c73ae168c89352fddd0f23020b2e4574860
Chad Hinds <chadrickj8@gmail.com>
2020-05-24 14:08:52 -0400

    Fix dotenv variable import issue

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
@@ -6,6 +7,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require('express-session')
 const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('./models/User')
 const Medicine = require('./models/Medicine')
 const cors = require("cors");
@@ -50,7 +52,21 @@ passport.deserializeUser(function (id, done) {
     });
 });
 
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     callbackURL: "http://localhost:3000/auth/google/secrets",
+//     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+// },
+//     function (accessToken, refreshToken, profile, cb) {
+//         console.log(profile);
 
+//         User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//             return cb(err, user);
+//         });
+//     }
+// ));
+console.log(process.env.CLIENT_SECRET)
 
 // ROUTES
 // Home Route
@@ -64,19 +80,6 @@ app.get('/', (req, res) => {
 // })
 // Register a new user profile
 app.post('/register', (req, res) => {
-    // User.register({ username: req.body.username}, req.body.password, (err, user)=>{
-    //     if(err) {
-    //         console.log(err)
-            
-    //         res.redirect("/register");
-    //     }
-    //     else{
-            // passport.authenticate("local")(req, res, ()=>{
-            //     // res.redirect("/profile")
-            //     res.send('Success!')
-            // })
-    //     }
-    // })
     User.register({ username: req.body.username }, req.body.password)
         .then(passport.authenticate("local", (req, res, () => {
             // res.redirect("/profile")
@@ -102,7 +105,13 @@ app.post('/login', (req, res) => {
         }
     })
 })
+// Logout the user
+app.get('/logout', (req, res)=>{
+    req.logout();
+    res.status(200).json({ msg: 'Logged out' });
+})
 
+// View User Profile
 app.post('/profile', (req, res)=>{
   
 })
