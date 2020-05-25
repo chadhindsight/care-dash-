c9f05049ecd416feefbbdf2ef394ec7719104c7d
Chad Hinds <chadrickj8@gmail.com>
2020-05-25 10:11:48 -0400

    Use some actions

---
+++ front-care/src/components/user/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import actions from '../../services/index';
+
+const Profile = () => {
+    return (
+        <div>
+           Profile 
+        </div>
+    );
+};
+
+export default Profile;
