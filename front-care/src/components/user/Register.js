fbea0086612e68ff80a95606452e18f755cbc4dc
Chad Hinds <chadrickj8@gmail.com>
2020-05-25 13:00:50 -0400

    latest

--- front-care/src/components/user/Register.js
+++ front-care/src/components/user/Register.js
@@ -9,20 +9,21 @@ const Register = (props) => {
     const [password, setPassword] = useState('')
     const [fullname, setFullName] = useState('')
     const [primaryPharmacy, setPharmacy] = useState('')
-    const [conditions, setCondition] = useState([])
-
+    const [conditions, setCondition] = useState('')
+    
+   
     const renderRedirect = () => <Redirect to='/profile' />
 
 // Handle submit passes the registration info to the related action and then that info  gets passed to backend DB
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const userInformation = {username, password, fullname, primaryPharmacy, conditions}
+        const userInformation = { username, fullname, primaryPharmacy, password, conditions}
         await actions.register(userInformation).then(user => {
             props.setUser({ ...user.data })
             renderRedirect()
         }).catch(err=> console.log('bad juju!'))
     }
-    console.log(username, 'username here')
+    console.log(conditions, 'username here')
     return (
         <>
             <form onSubmit={handleSubmit}>
@@ -30,7 +31,7 @@ const Register = (props) => {
                 <input type="text" name="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
                 <input type="text" name="fullname" placeholder="fullname" onChange={e => setFullName(e.target.value)} />
                 <input type="text" name="primaryPharmacy" placeholder="pharmacy" onChange={e => setPharmacy(e.target.value)} />
-                <input type="text" placeholder="symptoms/conditions"/>
+                <input type="text" placeholder="symptoms/conditions" onChange={e=> setCondition(e.target.value)}/>
                 <input type="submit"/>
             </form>
 
