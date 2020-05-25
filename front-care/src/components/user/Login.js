9d4ab2b46f51e6a21eb3c984806c3ddab5c74da8
Chad Hinds <chadrickj8@gmail.com>
2020-05-25 18:47:46 -0400

    adress display info bug

--- front-care/src/components/user/Login.js
+++ front-care/src/components/user/Login.js
@@ -8,21 +8,20 @@ const Login = (props) => {
 
     const logInHandler = async e => {
         e.preventDefault()
-        await actions.login({ username: username, password: password }).then(user => {
-            props.setUser({ ...user.data }) 
+        await actions.login({ username, password }).then(user => {
+            props.setUser({ ...user.data })
             props.history.push("/profile")
-
-        }).catch(({ response }) => console.error(response.data));
+        }).catch((err) => console.log(err));
     }
-   
+
     return (
         <>
             <form onSubmit={logInHandler}>
                 <input type="text" name="username" placeholder="username" onChange={e => setUserName(e.target.value)} />
                 <input type="text" name="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
-                <input type="submit"/>
+                <input type="submit" />
             </form>
-            
+
             <a className="btn btn-block btn-social btn-google" href="/auth/google" role="button">
                 <i className="fab fa-google"></i>
             Login with Google
