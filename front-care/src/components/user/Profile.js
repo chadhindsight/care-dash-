769ff4f6b82ce31a9474cb241cd7362a9a7c19b9
Chad Hinds <chadrickj8@gmail.com>
2020-05-25 14:30:31 -0400

    Set up rendering of props

--- front-care/src/components/user/Profile.js
+++ front-care/src/components/user/Profile.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import actions from '../../services/index';
 
-const Profile = () => {
+const Profile = (props) => {
+    console.log(props.profile)
     return (
-        <div>
+        <>
            Profile 
-        </div>
+            <h1>{props.profile.fullname}</h1>
+            <h2>{props.profile.primaryPharmacy}</h2>
+            <h3>{props.profile.conditions}</h3>
+        </>
     );
 };
 
