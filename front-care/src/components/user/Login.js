c8ee784f81f96e4006059ca7a2a4e7fd7952a7aa
Chad Hinds <chadrickj8@gmail.com>
2020-05-25 18:09:49 -0400

    Return user data on succeful login

--- front-care/src/components/user/Login.js
+++ front-care/src/components/user/Login.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import actions from '../../services/index';
 
-const Login = () => {
+const Login = (props) => {
     // REACT HOOKS HERE!
+    const [username, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+
+    const logInHandler = async e => {
+        e.preventDefault()
+        await actions.login({ username: username, password: password }).then(user => {
+            props.setUser({ ...user.data }) 
+            props.history.push("/profile")
+
+        }).catch(({ response }) => console.error(response.data));
+    }
+   
     return (
-        <div>
-            <a class="btn btn-block btn-social btn-google" href="/auth/google" role="button">
-                <i class="fab fa-google"></i>
+        <>
+            <form onSubmit={logInHandler}>
+                <input type="text" name="username" placeholder="username" onChange={e => setUserName(e.target.value)} />
+                <input type="text" name="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
+                <input type="submit"/>
+            </form>
+            
+            <a className="btn btn-block btn-social btn-google" href="/auth/google" role="button">
+                <i className="fab fa-google"></i>
             Login with Google
           </a>
-        </div>
+        </>
     );
 };
 
