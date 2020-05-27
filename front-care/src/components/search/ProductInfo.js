b3fa7b87f0fec2d32fa1f4209b252be6c5cbccdf
Chad Hinds <chadrickj8@gmail.com>
2020-05-27 15:57:58 -0400

    Remove non alphanumeric

--- front-care/src/components/search/ProductInfo.js
+++ front-care/src/components/search/ProductInfo.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-const ProductInfo = (props) => {
+const ProductInfo = (props) =>{
+    // Remove non-alphanumeric
+    let editedReview
+    props.currentProduct.review === undefined ? editedReview = ' ' : editedReview = props.currentProduct.review.replace(/[^0-9a-z]/gi, ' ')
+
     return (
         <div>
             <h2>{props.currentProduct.drugName}</h2>
             <p>Rating: {props.currentProduct.rating}</p>
-            <p>This treats: {props.currentProduct.condition}</p>
+            <p>For: {props.currentProduct.condition}</p>
+            <p>Review: {editedReview}</p>
             <button onClick={props.addToCart}>Add To Cart</button>
         </div>
-    );
-};
+        );
+    }
 export default ProductInfo;
