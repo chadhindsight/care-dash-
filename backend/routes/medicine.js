56a68d43256ef780ff57e12c5b99e993750c5b9b
Chad Hinds <chadrickj8@gmail.com>
2020-05-27 22:42:53 -0400

    Test to see if when an order is placed on the front it gets correctly sent to the backend

--- backend/routes/medicine.js
+++ backend/routes/medicine.js
@@ -6,16 +6,16 @@ const Medicine = require('../models/Medicine');
 // Search route
 router.get('/search', (req, res, next) => {
     // //DB SEARCH
-    console.log(req.query)
     Medicine.find({ drugName: { $regex: req.query.name, $options: 'i' }}).limit(20).then(meds => {
-        console.log(meds)
+        // console.log(meds)
         res.json({meds})
     })
         .catch(err => console.log("Cannot find medication"))
 
 })
 //Cart route
-router.get('/cart', (req, res, next) => {
-
+router.post('/order', (req, res, next) => {
+    //Target the specific user's order array
+    console.log(req.body)
 })
 module.exports = router;
