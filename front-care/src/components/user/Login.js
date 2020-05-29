f81e6d98d4d74b3750e1b03a2cf7d6a056b2d015
Chad Hinds <chadrickj8@gmail.com>
2020-05-28 20:41:35 -0400

    Fixed login bug

--- front-care/src/components/user/Login.js
+++ front-care/src/components/user/Login.js
@@ -3,21 +3,24 @@ import actions from '../../services/index';
 
 const Login = (props) => {
     // REACT HOOKS HERE!
-    const [username, setUserName] = useState('')
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const logInHandler = async e => {
         e.preventDefault()
-        await actions.login({ username, password }).then(user => {
+        console.log(email, password)
+        await actions.login({ email, password }).then(user => {
+            console.log(user, 'line 12')
             props.setUser({ ...user.data })
             props.history.push("/profile")
         }).catch((err) => console.log(err));
     }
+console.log(email, password)
 
     return (
         <>
             <form onSubmit={logInHandler}>
-                <input type="text" name="username" placeholder="username" onChange={e => setUserName(e.target.value)} />
+                <input type="text" name="email" placeholder="username" onChange={e => setEmail(e.target.value)} />
                 <input type="text" name="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
                 <input type="submit" />
             </form>
