660e1341e1877087ab5edf3d911538f3f0f5585f
Chad Hinds <chadrickj8@gmail.com>
2020-06-04 10:52:30 -0400

    Add uniqueID as the key for iterable items

--- front-care/src/components/user/Profile.js
+++ front-care/src/components/user/Profile.js
@@ -1,33 +1,32 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import actions from '../../services/index';
 import { MDBBtn } from 'mdbreact';
 
 const Profile = (props) => {
-    
-   const  profileSubmit = async (e) => {
+
+    const profileSubmit = async (e) => {
         e.preventDefault()
         console.log("made it to submit", props.profile)
         let newProfile = await actions.editProfile(props.profile)
 
         console.log(newProfile)
     }
-   
-    useEffect(() =>{
-          return async () => {
-            let order = await props.profile.order
-            console.log(props)
-        }
-    })
 
+    
+    const showOrder = () => {
+        return props?.profile?.order?.map(med => {
+            return <li key={med.uniqueID}>{med.drugName}</li>
+        })
+    }
     return (
         <>
-           Profile 
+            Profile
             <h1>{props.profile.email}</h1>
-                {/* <ul>{showOrder()}</ul> */}
-            <form onSubmit={(e)=>profileSubmit(e)}>
-                <input placeholder="email" name="email" onBlur={props.handleBlur} onChange={ props.handleChange} /> 
-                <input placeholder="primary pharmacy" name="primaryPharm" onBlur={props.handleBlur} onChange={props.handleChange} /> 
-                
+            <ul>{showOrder()}</ul>
+            <form onSubmit={(e) => profileSubmit(e)}>
+                <input placeholder="email" name="email" onBlur={props.handleBlur} onChange={props.handleChange} />
+                <input placeholder="primary pharmacy" name="primaryPharm" onBlur={props.handleBlur} onChange={props.handleChange} />
+
                 <MDBBtn color="secondary" onClick={profileSubmit}>Submit</MDBBtn>
             </form>
         </>
@@ -35,4 +34,3 @@ const Profile = (props) => {
 };
 
 export default Profile;
-// dayjs
