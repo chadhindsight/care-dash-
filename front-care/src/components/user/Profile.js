c3405c5dc52925ef218f3b97df1d0e31a41d6b82
Chad Hinds <chadrickj8@gmail.com>
2020-06-14 17:52:39 -0400

    Add product rating to the section that displays a user orders

--- front-care/src/components/user/Profile.js
+++ front-care/src/components/user/Profile.js
@@ -4,7 +4,7 @@ import { MDBBtn } from 'mdbreact';
 
 const Profile = (props) => {
 
-    const profileSubmit = async (e) => {
+    const profileEditSubmit = async (e) => {
         e.preventDefault()
         console.log("made it to submit", props.profile)
         let newProfile = await actions.editProfile(props.profile)
@@ -12,21 +12,24 @@ const Profile = (props) => {
         console.log(newProfile)
     }
 
-    
+    console.log(props.profile.order)    
     const showOrder = () => {
         return props?.profile?.order?.map(med => {
-            return <li key={med.uniqueID}>{med.drugName}</li>
+            return <li key={med.uniqueID}>
+                <h3>{med.drugName}</h3>
+                <p>Rating: {med.rating}</p>
+                </li>
         })
     }
     return (
         <>
             <h1>{props.profile.email}</h1>
             <ul>{showOrder()}</ul>
-            <form onSubmit={(e) => profileSubmit(e)}>
+            <form onSubmit={(e) => profileEditSubmit(e)}>
                 <input placeholder="email" name="email" onBlur={props.handleBlur} onChange={props.handleChange} />
                 <input placeholder="primary pharmacy" name="primaryPharm" onBlur={props.handleBlur} onChange={props.handleChange} />
 
-                <MDBBtn color="secondary" onClick={profileSubmit}>Submit</MDBBtn>
+                <MDBBtn color="secondary" onClick={profileEditSubmit}>Submit</MDBBtn>
             </form>
         </>
     );
