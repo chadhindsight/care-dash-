5ef0c98011074a34eb089af9276866f3bdd72974
Chad Hinds <chadrickj8@gmail.com>
2020-06-20 10:02:14 -0400

    Add footer

---
+++ front-care/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            Copyright © 2020 - All Rights Reserved - Chadrick Hinds
+        </div>
+    );
+};
+
+export default Footer;
