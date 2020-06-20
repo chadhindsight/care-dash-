43e660f5e1a809fe6faa82fa49bb550b69cc5219
Chad Hinds <chadrickj8@gmail.com>
2020-06-20 10:08:04 -0400

    Fix typo in date logic

--- front-care/src/components/Footer.js
+++ front-care/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            Copyright © 2020 - All Rights Reserved - Chadrick Hinds
+            Copyright © {new Date().getFullYear()} - All Rights Reserved - Chadrick Hinds
         </div>
     );
 };
