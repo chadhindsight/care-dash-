09eda1931ed630ab5b5c0f9ffb8ceedf9c06c6c6
Chad Hinds <chadrickj8@gmail.com>
2020-06-30 15:48:24 -0400

    More footer adjustments and text styling

--- front-care/src/components/Footer.js
+++ front-care/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createFromIconfontCN } from '@ant-design/icons';
+import { createFromIconfontCN, LinkedinFilled, LinkOutlined } from '@ant-design/icons';
 
 const Footer = () => {
     const IconFont = createFromIconfontCN({
@@ -8,15 +8,32 @@ const Footer = () => {
 
     return (
         <footer>
-            Copyright © Chad Hinds {new Date().getFullYear()} - All Rights Reserved - CareDash
+            <h5 id="footer-text">Copyright © Chad Hinds {new Date().getFullYear()} - All Rights Reserved - CareDash</h5>
            
-            <ul class="social-links">
-                <li>
-                    <a href="https://twitter.com/Chadhindsight" rel="noopener noreferrer" target="_blank"> 
-                    <IconFont type="icon-twitter" />
-                    </a>
-                </li>
-            </ul>
+           <section className="ass">
+                <ul class="social-links">
+                    <li>
+                        <a href="http://chadhindsight.github.io" rel="noopener noreferrer" target="_blank">
+                            <LinkOutlined />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/Chadhindsight" rel="noopener noreferrer" target="_blank">
+                            <IconFont type="icon-twitter" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/chadrick-hinds" rel="noopener noreferrer" target="_blank">
+                            <LinkedinFilled />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/chadhindsight" rel="noopener noreferrer" target="_blank">
+                            <IconFont type="icon-github" />
+                        </a>
+                    </li>
+                </ul>
+           </section>
         </footer>
     );
 };
