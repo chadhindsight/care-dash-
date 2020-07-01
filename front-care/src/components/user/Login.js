77b4939e6e02681e079a300001d655c6e880e5cc
Chad Hinds <chadrickj8@gmail.com>
2020-06-30 22:25:53 -0400

    Make login a card

--- front-care/src/components/user/Login.js
+++ front-care/src/components/user/Login.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import actions from '../../services/index';
-import {  MDBBtn } from 'mdbreact';
-import {Link} from 'react-router-dom'
+import { MDBBtn} from 'mdbreact';
+import {Link} from 'react-router-dom';
+import { Card } from 'antd';
 import { ReactComponent as Signin } from '../../assets/signin.svg'
 
 const Login = (props) => {
@@ -20,24 +21,23 @@ const Login = (props) => {
     }
 
     return (
-        <div className="login-back">
-            <form onSubmit={logInHandler} className="log">
-                <span className="symbol-input100">
-                    <i className="fa fa-envelope" aria-hidden="true"></i>
-                </span>
-                <input className="input100" type="text" name="email" placeholder="username" onChange={e => setEmail(e.target.value)} />
-                <input className="input100" type="text" name="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
-                <span className="symbol-input200">
-                    <i className="fa fa-lock" aria-hidden="true"></i>
-                </span>
-                <Link to="/profile"><MDBBtn color="secondary"  className="login100-form-btn"
-                onClick={logInHandler}>Submit</MDBBtn></Link>
-                <Signin style={{ width: '40%'}}
-                    className="background-pic-login"/>
-            </form>
+        <form>
+            <Card onSubmit={logInHandler} hoverable className="log-card">
+                <input type="text" name="email" placeholder="username" onChange={e => setEmail(e.target.value)}
+                className="login-input"/>
+                <input type="text" name="password" placeholder="password" onChange={e => setPassword(e.target.value)}
+                className="login-input"/>
 
-        </div>
+                <Link to="/profile"><MDBBtn color="secondary"  className="login100-Form-btn"
+                onClick={logInHandler}>Submit</MDBBtn></Link>
+                {/* <Signin style={{ width: '40%'}}
+                    className="background-pic-login"/> */}
+            </Card>
+        </form>
     );
 };
+
+{/* <input className="input100" type="text" name="email" placeholder="username" onChange={e => setEmail(e.target.value)} />
+    <input className="input100" type="text" name="password" placeholder="password" onChange={e => setPassword(e.target.value)} /> */}
 
 export default Login;
