86a6c1c1438bfc3243f7acab3e2c150caa63d21a
Chad Hinds <chadrickj8@gmail.com>
2020-07-15 21:00:22 -0400

    Recent update

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["https://care-dash.netlify.app"] 
+    origin: ["http://localhost:3000", "https://care-dash.netlify.app"] 
   })
 );
 
