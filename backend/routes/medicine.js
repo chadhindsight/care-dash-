e1a5cc5f16b0a7fe85f3d621018c8d02e0dcb607
Chad Hinds <chadrickj8@gmail.com>
2020-08-06 11:19:07 -0400

    Fix bug that in medicine search route

--- backend/routes/medicine.js
+++ backend/routes/medicine.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Medicine = require('../models/Medicine')
 const passport = require('../config/passport');
 require('dotenv').config();
 
 // Search route
 router.get('/search', (req, res, next) => {
     // //DB SEARCH HERE
-    Medicine.find({ drugName: { $regex: req.query.name, $options: 'i' }}).limit(20).then(meds => {
-        res.json({meds})
+    Medicine.find({ drugName: { $regex: req.query.name, $options: 'i' } }).limit(20).then(meds => {
+        res.json({ meds })
     })
         .catch(err => console.log("Cannot find medication"))
 
