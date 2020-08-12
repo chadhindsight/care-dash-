e2c7d8f74e5cb6fac3384372337f864f64da6e20
Chad Hinds <chadrickj8@gmail.com>
2020-08-12 11:45:44 -0400

    Prep for custom tests

--- front-care/src/App.test.js
+++ front-care/src/App.test.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders  link', () => {
-  const { getByText } = 
-  render(<BrowserRouter>
-    <App />
-  </BrowserRouter>);
-  const linkElement = getByText(/Copyright/i);
-  expect(linkElement).toBeInTheDocument();
+
 });
 
 
