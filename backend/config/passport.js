bcdaa1aba34049dd2c2fcda0e60b5ca5ff124529
Chad Hinds <chadrickj8@gmail.com>
2021-01-18 12:37:54 -0500

    comment on how createStrategy is related to mongoose and passport hooking up

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const passport = require('passport');
 
-// This createStrategy() line is related to mongoose and passport hooking up
+// This createStrategy() line is related to mongoose and passport hooking up.
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
