e62bda22922afd74b4c51a2d13d5f92df0325231
Chad Hinds <chadrickj8@gmail.com>
2021-01-22 13:20:06 -0500

    Fix .gitignore issues

--- front-care/src/components/Order.js
+++ front-care/src/components/Order.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Modal, Button } from 'antd';
 import actions from '../services/index';
 import { MDBBtn } from "mdbreact";
+import { ReactComponent as Delivery } from '../assets/delivery.svg'
+
 import {Link} from 'react-router-dom';
 
 const Order = (props) => {
@@ -17,9 +19,11 @@ const Order = (props) => {
 
     return (
         <div>
+            <Delivery style={{ position: 'relative', zIndex: '-1', marginBottom: '-5%', width: '50%', height: '50%' }} />
             <form>
                 <ul>{displayItems}</ul>
-                <MDBBtn className="default" onClick={() => props.onOrderSubmit(props.cart) }>Place Order</MDBBtn>
+                <MDBBtn className="default" style={{ position: 'relative', marginBottom: '55%'}}
+                onClick={() => props.onOrderSubmit(props.cart) }>Place Order</MDBBtn>
             </form>
         </div>
     );
