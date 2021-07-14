4e4cc0f0025d3996bf5907888aef30a3a46a3945
Chad Hinds <chadrickj8@gmail.com>
2021-07-14 10:20:47 -0400

    remove passport import as it was not being used

--- backend/routes/medicine.js
+++ backend/routes/medicine.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Medicine = require('../models/Medicine');
-const passport = require('../config/passport');
 require('dotenv').config();
 
 // Search route
